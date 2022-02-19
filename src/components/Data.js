83b5c93eb49fb20055cdc98d49efdd450118b2cc
Adem Baroudi <adembaroudi3177@gmail.com>
2022-02-19 17:03:59 +0100

    up

---
+++ src/components/Data.js
@@ -0,0 +1,23 @@
+export const features = [
+    {
+      title: "React",
+      description:
+        "it's a library what we used to build my portfolio",
+      image: "https://logos-download.com/wp-content/uploads/2016/09/React_logo_wordmark.png",
+     
+    },
+    {
+      title: "CSS Tailwind",
+      description:
+        "it's a module what we install it to control user interface",
+      image: "https://www.troispointzero.fr/content/uploads/2020/03/tailwind-pre.png",
+ 
+    },
+    {
+      title: "Google map",
+      description:
+        "i have insered it in my portfolio ",
+      image: "https://w7.pngwing.com/pngs/811/739/png-transparent-google-maps-google-map-maker-web-mapping-travel-services-logo-sign-internet.png",
+
+    },
+  ];
